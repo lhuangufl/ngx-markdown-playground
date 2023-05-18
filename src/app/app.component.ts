ef9cd7e8e23b22f0bf5bcfd0e21b15e9f672e75e
lhuangufl <lhuangufl@gmail.com>
2023-05-18 10:02:55 -0400

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
@@ -8,14 +9,13 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent {
   angularVersion = VERSION.full;
   ngxMarkdownVersion = '16.0.0';
-
-  markdown = `
-
-  | Column 1 | Column 2   | Column 3                    | Column 4                |
-  | -------- | ---------- | --------------------------- | ----------------------- |
-  | 4.2.5    | 4.2.5.1    | FRTB Financial Topic        | Rule related to FRTB     |
-  |   |   | | Another rule related to FRTB |
-  |   |   | | ![Alt Text](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf7Q3xKebsZM5cmlyvTBtcboCmdQkao-IxHOYK_wlu2g&s) |
-  |   |   | |  |
-  `;
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http
+      .get('../assets/data.md', { responseType: 'text' })
+      .subscribe((data) => {
+        this.markdown = data;
+      });
+  }
+  markdown: string;
 }
