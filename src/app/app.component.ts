4120fa5bbe8953e526747d934a8e368066de7760
lhuangufl <lhuangufl@gmail.com>
2023-05-18 08:22:24 -0400

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,13 @@ export class AppComponent {
   angularVersion = VERSION.full;
   ngxMarkdownVersion = '16.0.0';
 
-  markdown = `## Markdown __rulez__!
----
+  markdown = `
 
-### Syntax highlight
-\`\`\`typescript
-const language = 'typescript';
-\`\`\`
-
-### Lists
-1. Ordered list
-2. Another bullet point
-   - Unordered list
-   - Another unordered bullet
-
-### Blockquote
-> Blockquote to the max`;
+  | Column 1 | Column 2   | Column 3                    | Column 4                |
+  | -------- | ---------- | --------------------------- | ----------------------- |
+  | 4.2.5    | 4.2.5.1    | FRTB Financial Topic        | Rule related to FRTB     |
+  |   |   | | Another rule related to FRTB |
+  |   |   | | ![Alt Text](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf7Q3xKebsZM5cmlyvTBtcboCmdQkao-IxHOYK_wlu2g&s) |
+  |   |   | |  |
+  `;
 }
