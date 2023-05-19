f86f22263b33b24ece1885dd5b913c816f24748d
lhuangufl <lhuangufl@gmail.com>
2023-05-19 14:25:56 -0400

    5 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FileAttachment } from '@observablehq/stdlib';
+import * as Plot from '@observablehq/plot';
+import * as _ from 'lodash';
+import $ from 'jquery';
+import * as d3 from 'd3';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -7,38 +13,105 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  markdown_origin: string = '';
   markdown: string = '';
+  chartRef;
+  data;
+  svg;
+  timer;
+  update() {
+    this.plot();
+  }
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.chartRef = $('.chart');
+  }
+
+  async ngOnInit() {
     const repository = 'lhuangufl/ngx-markdown-playground';
     const filePath = 'README.md';
     const textPath = 'text1.txt';
     const url = `https://api.github.com/repos/${repository}/contents/${filePath}`;
     const txturl = `https://api.github.com/repos/${repository}/contents/${textPath}`;
+
     this.http.get(url).subscribe(
       (response: any) => {
         const content = response.content;
 
-        this.markdown = atob(content); // Decode base64 content
+        this.markdown_origin = atob(content); // Decode base64 content
       },
       (error: any) => {
         console.error('Error occurred while reading the file:', error);
       }
     );
+
     this.http.get(txturl).subscribe(
       (response: any) => {
         const content = response.content;
 
         const filename = atob(content).replace(/\n/g, '<br>');
-        console.log(filename);
-        this.markdown = this.markdown.replace('${filename}', filename);
-        console.log(this.markdown);
+        this.markdown = this.markdown_origin.replace('${filepath}', filename);
       },
       (error: any) => {
         console.error('Error occurred while reading the file:', error);
       }
     );
+
+    await this.fetchData();
+    this.plot();
+  }
+
+  fetchData(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.http
+        .get('./assets/data.csv', { responseType: 'text' })
+        .pipe(
+          map((response: string) => {
+            const rows = response.split('\n');
+            const headers = rows[0].split(',');
+
+            // Parse each row as an object
+            return rows.slice(1).map((row) => {
+              const values = row.split(',');
+              const date = values[0];
+              const value = parseFloat(values[1]);
+
+              return {
+                date,
+                value,
+              };
+            });
+          })
+        )
+        .subscribe(
+          (data) => {
+            this.data = data;
+            resolve(); // Resolve the Promise when data is assigned
+          },
+          (error) => {
+            reject(error); // Reject the Promise if there's an error
+          }
+        );
+    });
+  }
+
+  plot() {
+    const svg = Plot.plot({
+      style: {
+        background: 'transparent',
+      },
+      y: {
+        grid: true,
+      },
+      color: {
+        type: 'diverging',
+        scheme: 'burd',
+      },
+      marks: [Plot.dot(this.data, { x: 'date', y: 'value' })],
+    });
+
+    this.chartRef.html(svg);
   }
 }
