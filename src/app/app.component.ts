53383acb02a9e9e8bbcb178402ad431c769aae16
lhuangufl <lhuangufl@gmail.com>
2023-05-19 09:00:22 -0400

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,27 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     const repository = 'lhuangufl/ngx-markdown-playground';
     const filePath = 'README.md';
+    const textPath = 'text1.txt';
     const url = `https://api.github.com/repos/${repository}/contents/${filePath}`;
-
+    const txturl = `https://api.github.com/repos/${repository}/contents/${textPath}`;
     this.http.get(url).subscribe(
       (response: any) => {
         const content = response.content;
-        
+
         this.markdown = atob(content); // Decode base64 content
+      },
+      (error: any) => {
+        console.error('Error occurred while reading the file:', error);
+      }
+    );
+    this.http.get(txturl).subscribe(
+      (response: any) => {
+        const content = response.content;
+
+        const filename = atob(content).replace(/\n/g, '<br>');
+        console.log(filename);
+        this.markdown = this.markdown.replace('${filename}', filename);
+        console.log(this.markdown);
       },
       (error: any) => {
         console.error('Error occurred while reading the file:', error);
